4392ef54eeb9ea8fd233b4b55f12bd0656692296
cainmaila <cainmaila@gmail.com>
2021-08-31 15:52:31 +0800

    save

--- src/components/editor/DetailUi/index.vue
+++ src/components/editor/DetailUi/index.vue
@@ -1,16 +1,36 @@
 <template>
-  <div id="DetailUi" v-if="type > 0">
+  <div id="DetailUi" v-if="type > 0" :class="{ close: !tab }">
     <ScopeAreaMod v-bind="$attrs" v-if="type === 1" :scale="scale" />
     <PartAreaMod v-else-if="type === 2" v-bind="$attrs" />
+    <div class="x">
+      <At2IconBtn
+        icon="close"
+        width="20"
+        height="20"
+        color="#000"
+        @on-click="onTab"
+      />
+    </div>
   </div>
 </template>
 <script>
+import At2IconBtn from '~at2@/components/At2IconBtn'
 import ScopeAreaMod from './ScopeAreaMod'
 import PartAreaMod from './PartAreaMod'
 export default {
   name: 'DetailUi',
-  components: { ScopeAreaMod, PartAreaMod },
+  components: { At2IconBtn, ScopeAreaMod, PartAreaMod },
   props: ['type', 'scale'],
+  data() {
+    return {
+      tab: true,
+    }
+  },
+  methods: {
+    onTab() {
+      this.tab = !this.tab
+    },
+  },
 }
 </script>
 <style lang="postcss" scoped>
@@ -23,5 +43,14 @@ export default {
   left: 0;
   top: 0;
   background: #fff;
+  transform: translate3d(0, 0, 0);
+  &.close {
+    transform: translate3d(-200px, 0, 0);
+  }
+}
+.x {
+  position: absolute;
+  top: 0px;
+  right: -20px;
 }
 </style>
