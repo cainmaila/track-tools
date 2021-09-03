8acf330f614ad7ca0ed9aac0d8e0b10039764d8f
cainmaila <cainmaila@gmail.com>
2021-09-03 13:02:15 +0800

    save

--- src/Editor/metaInSetHeadler.js
+++ src/Editor/metaInSetHeadler.js
@@ -3,51 +3,52 @@ import { keyToUnit, mToUnit, mToPx } from '@/tools/unitTools'
 import { hexToNumber } from '@/tools/colorTools'
 function metaInSetHeadler(original, scopeAreaData, viewportRef) {
   const setAreaMeta = data => {
-    scopeAreaData.tag = data.total_area.name //tag
-    scopeAreaData.color = data.total_area.frame_color //tag
-    scopeAreaData.direction = data.total_area.mn_angle //方位角
-    scopeAreaData.elevation = mToUnit(data.total_area.high, scopeAreaData.unit) //標高
-    scopeAreaData.unit = keyToUnit(data.total_area.display_unit) //單位
+    const { scale, total_area } = data
+    const unit = keyToUnit(total_area.display_unit) //單位
+    const initial_point_offsetX = total_area.initial_point_offset.x
+    const initial_point_offsetY = total_area.initial_point_offset.y
+    scopeAreaData.tag = total_area.name //tag
+    scopeAreaData.color = total_area.frame_color //tag
+    scopeAreaData.direction = total_area.mn_angle //方位角
+    scopeAreaData.elevation = mToUnit(total_area.high, unit) //標高
+    scopeAreaData.unit = unit //單位
     scopeAreaData.realWidth = mToUnit(
       /* 真實寬 */
-      data.total_area.length,
-      scopeAreaData.unit,
+      total_area.length,
+      unit,
     )
     //付現總區域
     original.mode = 'scope'
     viewportRef.value.createArea(
       {
-        x: data.total_area.initial_point_offset.x,
-        y: data.total_area.initial_point_offset.y,
-        w: mToPx(data.total_area.length, data.scale, scopeAreaData.unit),
-        h: mToPx(data.total_area.width, data.scale, scopeAreaData.unit),
+        x: initial_point_offsetX,
+        y: initial_point_offsetY,
+        w: mToPx(total_area.length, scale, unit),
+        h: mToPx(total_area.width, scale, unit),
       },
       {
-        tag: data.total_area.name,
+        tag: total_area.name,
       },
     )
 
     //房間區域
     data.area.forEach(area => {
       original.mode = 'area'
+      const color = hexToNumber(area.frame_color)
       const roomArea = viewportRef.value.createArea(
         {
-          x:
-            mToPx(area.pos_left_up.x, data.scale, scopeAreaData.unit) +
-            data.total_area.initial_point_offset.x,
-          y:
-            mToPx(area.pos_left_up.y, data.scale, scopeAreaData.unit) +
-            data.total_area.initial_point_offset.y,
-          w: mToPx(area.length, data.scale, scopeAreaData.unit),
-          h: mToPx(area.width, data.scale, scopeAreaData.unit),
+          x: mToPx(area.pos_left_up.x, scale, unit) + initial_point_offsetX,
+          y: mToPx(area.pos_left_up.y, scale, unit) + initial_point_offsetY,
+          w: mToPx(area.length, scale, unit),
+          h: mToPx(area.width, scale, unit),
         },
         {
           tag: area.name || '',
-          lineColor: hexToNumber(area.frame_color),
-          fillColor: hexToNumber(area.frame_color),
+          lineColor: color,
+          fillColor: color,
         },
       )
-      roomArea.userData.spaceHeight = mToUnit(area.high, scopeAreaData.unit)
+      roomArea.userData.spaceHeight = mToUnit(area.high, unit)
     })
 
     viewportRef.value.targetObj = null
