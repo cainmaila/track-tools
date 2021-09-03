5042a6528e831f05cf96ae73200923c69b728fd0
cainmaila <cainmaila@gmail.com>
2021-09-03 12:42:22 +0800

    付現

--- src/Editor/index.vue
+++ src/Editor/index.vue
@@ -37,6 +37,7 @@ import FootUi from '@/components/editor/FootUi'
 import LayerUi from '@/components/editor/LayerUi'
 import DetailUi from '@/components/editor/DetailUi'
 import { keyToUnit, mToUnit, mToPx } from '@/tools/unitTools'
+import { hexToNumber } from '@/tools/colorTools'
 export default {
   name: 'AppEditor',
   components: { FootUi, LayerUi, DetailUi },
@@ -102,6 +103,8 @@ export default {
         getAreaMeta,
         setAreaMeta: data => {
           console.log('#setAreaMeta', data)
+          scopeAreaData.tag = data.total_area.name //tag
+          scopeAreaData.color = data.total_area.frame_color //tag
           scopeAreaData.direction = data.total_area.mn_angle //方位角
           scopeAreaData.elevation = mToUnit(
             data.total_area.high,
@@ -116,11 +119,45 @@ export default {
           //付現總區域
           original.mode = 'scope'
 
-          viewportRef.value.createArea({
-            w: mToPx(data.total_area.length, data.scale, scopeAreaData.unit),
-            h: mToPx(data.total_area.width, data.scale, scopeAreaData.unit),
-            ...data.total_area.initial_point_offset,
+          viewportRef.value.createArea(
+            {
+              x: data.total_area.initial_point_offset.x,
+              y: data.total_area.initial_point_offset.y,
+              w: mToPx(data.total_area.length, data.scale, scopeAreaData.unit),
+              h: mToPx(data.total_area.width, data.scale, scopeAreaData.unit),
+            },
+            {
+              tag: data.total_area.name,
+            },
+          )
+
+          //房間區域
+          data.area.forEach(area => {
+            original.mode = 'area'
+            const roomArea = viewportRef.value.createArea(
+              {
+                x:
+                  mToPx(area.pos_left_up.x, data.scale, scopeAreaData.unit) +
+                  data.total_area.initial_point_offset.x,
+                y:
+                  mToPx(area.pos_left_up.y, data.scale, scopeAreaData.unit) +
+                  data.total_area.initial_point_offset.y,
+                w: mToPx(area.length, data.scale, scopeAreaData.unit),
+                h: mToPx(area.width, data.scale, scopeAreaData.unit),
+              },
+              {
+                tag: area.name || '',
+                lineColor: hexToNumber(area.frame_color),
+                fillColor: hexToNumber(area.frame_color),
+              },
+            )
+            roomArea.userData.spaceHeight = mToUnit(
+              area.high,
+              scopeAreaData.unit,
+            )
           })
+
+          viewportRef.value.targetObj = null
         },
       })
       postEvent('ready')
@@ -137,7 +174,7 @@ export default {
             break
           case 'area':
             original.mode = 'sel'
-            area.tag = 'Area'
+            area.tag || (area.tag = 'Area')
             original.selAeeaType = 2
             break
         }
