77509d0ff44e24bf64789cc9bee8aa3b702f94f7
cainmaila <cainmaila@gmail.com>
2021-09-13 10:02:48 +0800

    historyStore

--- src/TrackPlayer/player-history-handler.js
+++ src/TrackPlayer/player-history-handler.js
@@ -1,10 +1,17 @@
 import DrawPathPlayHelper from '@/tools/draw-lib/DrawPathViewport/DrawPathPlayHelper'
+import { reactive, ref } from 'vue'
 function playerHistoryHandler(viewPortRef) {
+  const historyHistoryRef = ref(null)
+  const historyStore = reactive({
+    totaleTime: 0, //總時間ms
+  })
   const setHistory = history => {
     const halper = new DrawPathPlayHelper(viewPortRef.value)
     halper.setHistory(history)
+    historyStore.totaleTime = halper.totaleTime
+    historyHistoryRef.value = halper
   }
-  return { setHistory }
+  return { historyStore, setHistory }
 }
 
 export default playerHistoryHandler
