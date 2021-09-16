287bcdbf20f1f633adbdb4ecc278623b0375dab4
cainmaila <cainmaila@gmail.com>
2021-09-16 14:57:12 +0800

    time

---
+++ src/tools/time-format.js
@@ -0,0 +1,10 @@
+function msToTime(ms) {
+  let _time = ~~(ms / 1000) //to s
+  const s = _time % 60
+  _time = ~~(_time / 60) //to m
+  const m = _time % 60
+  const h = _time % 60
+  return `${h ? h + ':' : ''}${m + ':'}${s}`
+}
+
+export { msToTime }
