d2c76adf06d57cd623dda7b79b6375f80d7108f4
cainmaila <cainmaila@gmail.com>
2021-09-27 11:01:37 +0800

    存檔UI移至編輯器tool

---
+++ src/components/editor/TopUi.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="top-ui">
+    <div><slot /></div>
+    <div class="btns">
+      <At2Btn
+        type=""
+        @on-click="$emit('ui', 'exit')"
+        :border="true"
+        :disabled="false"
+        pill
+        size="26"
+      >
+        離開
+      </At2Btn>
+      <At2Btn
+        v-if="save"
+        @on-click="$emit('ui', 'save')"
+        type=""
+        :border="false"
+        :disabled="false"
+        pill
+        size="26"
+      >
+        儲存
+      </At2Btn>
+    </div>
+  </div>
+</template>
+<script>
+import At2Btn from '~at2@/components/At2Btn'
+export default {
+  name: 'TopUi',
+  components: { At2Btn },
+  props: ['save'],
+  data() {
+    return {}
+  },
+}
+</script>
+<style lang="postcss" scoped>
+.top-ui {
+  width: 320px;
+  height: 40px;
+  padding: 0 20px;
+  background: rgb(233, 233, 233);
+  border-radius: 8px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  transform: translate3d(-50%, 0, 0);
+}
+.btns {
+  display: flex;
+  & .at2-btn {
+    margin-left: 15px;
+  }
+}
+</style>
