45f893c2a1bf5d41dfd19ebb37a6ec3746a8a2c3
cainmaila <cainmaila@gmail.com>
2021-09-30 18:17:04 +0800

    換icon

--- src/components/trackPlayer/PointTimeInfo.vue
+++ src/components/trackPlayer/PointTimeInfo.vue
@@ -1,21 +1,14 @@
 <template>
   <div class="point-time-info flex-center" v-show="time">
-    <At2Icon
-      type="entityColor"
-      width="14"
-      height="14"
-      color="rgb(0, 145, 255)"
-    />當前位置
+    <img src="img/po.png" width="14" height="14" /> 當前位置
     {{ dateStr }}
   </div>
 </template>
 <script>
-import At2Icon from '~at2@/components/At2Icon'
 import { toDateAndTime } from '@/tools/time-format'
 export default {
   name: 'PointTimeInfo',
   props: ['time'],
-  components: { At2Icon },
   computed: {
     dateStr() {
       return this.time ? toDateAndTime(this.time * 1) : ''
