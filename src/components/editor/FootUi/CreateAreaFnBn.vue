e8e9004877aeaa3133e0516dea58d350cfd0896b
cainmaila <cainmaila@gmail.com>
2021-10-05 13:03:28 +0800

    下方樣式

--- src/components/editor/FootUi/CreateAreaFnBn.vue
+++ src/components/editor/FootUi/CreateAreaFnBn.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="create-bn flex-center">
     <div
-      class="flex-center fn-bg"
+      class="flex-center fn-bg-1"
       :class="{ disabled: step === 2, bn: step === 1, acc: isScope }"
       @pointerup.stop="step === 1 && $emit('press', 'scope')"
     >
@@ -9,13 +9,14 @@
       <AddStateBtn :state="step === 1 ? 'none' : 'ok'" />
     </div>
     <div
-      class="flex-center fn-bg"
+      class="flex-center fn-bg-2"
       :class="{ disabled: step === 1, bn: step === 2, acc: isArea }"
       @pointerup.stop="step === 2 && $emit('press', 'area')"
     >
       <div class="txt">{{ $t('Editor.FoottUi.CreateAreaFnBn.area') }}</div>
       <AddStateBtn v-if="step === 2" />
     </div>
+    <div class="trd" :class="{ disabled: step === 2, acc: isScope }"></div>
   </div>
 </template>
 <script>
@@ -39,6 +40,7 @@ export default {
 </script>
 <style lang="postcss" scoped>
 .create-bn {
+  position: relative;
   width: 340px;
   height: calc(55px + 15px);
   background: #000;
@@ -48,15 +50,35 @@ export default {
   justify-content: space-around;
   color: #fff;
 }
-.disabled {
+/* .disabled {
   background: #888;
   & .txt {
     color: #ddd;
   }
-}
-.fn-bg {
-  width: 50%;
+} */
+.fn-bg-1 {
+  width: 47%;
   height: 100%;
+  background: #000;
+  color: #fff;
+  &.disabled {
+    background: #6e6d6d;
+    color: #969696;
+  }
+  &.acc {
+    background: #5595ef;
+  }
+}
+.fn-bg-2 {
+  width: 53%;
+  height: 100%;
+  background: #444;
+  &.acc {
+    background: #5595ef;
+  }
+  &.disabled {
+    color: #aaa;
+  }
 }
 .txt {
   width: 60px;
@@ -64,7 +86,19 @@ export default {
   font-size: 12px;
   font-weight: 500;
 }
-.acc {
-  background: #5595ef;
+.trd {
+  position: absolute;
+  transform: translateX(2px);
+  width: 0;
+  height: 0;
+  border-style: solid;
+  border-width: 35px 0 35px 25px;
+  border-color: transparent transparent transparent #000;
+  &.disabled {
+    border-color: transparent transparent transparent #6e6d6d;
+  }
+  &.acc {
+    border-color: transparent transparent transparent #5595ef;
+  }
 }
 </style>
