586c8dcfeb6d8e4140e1070426a7250c3f6332f2
cainmaila <cainmaila@gmail.com>
2021-10-07 16:25:43 +0800

    第一次的多語

--- src/pulgIn-i18n.js
+++ src/pulgIn-i18n.js
@@ -1,6 +1,6 @@
 import { createI18n } from 'vue-i18n'
-import en from './locale/en.json'
-import zhTW from './locale/zh-TW.json'
+import en from './track-tools-locale/en.json'
+import zhTW from './track-tools-locale/zh-TW.json'
 export default createI18n({
   // legacy: false,
   locale: window.navigator.language, // set locale
