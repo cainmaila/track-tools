fc2eb55f19193a8f98e5adb97691b6add310a8a3
cainmaila <cainmaila@gmail.com>
2021-10-12 10:17:06 +0800

    多語系修正

--- src/pulgIn-i18n.js
+++ src/pulgIn-i18n.js
@@ -3,9 +3,10 @@ import en from './track-tools-locale/en.json'
 import zhTW from './track-tools-locale/zh-TW.json'
 import cn from './track-tools-locale/cn.json'
 import ja from './track-tools-locale/ja.json'
+const urlParams = new URLSearchParams(window.location.search)
 export default createI18n({
   // legacy: false,
-  locale: window.navigator.language, // set locale
+  locale: urlParams.get('lang') || 'en', // set locale
   fallbackLocale: 'en', // set fallback locale
   messages: {
     'zh-TW': zhTW,
