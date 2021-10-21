1ad4033578c39391c2a52fc6f1979a34e606aca0
cainmaila <cainmaila@gmail.com>
2021-10-21 11:33:05 +0800

    fix i18n

--- src/main-track-player.js
+++ src/main-track-player.js
@@ -1,5 +1,9 @@
 import '@/assets/css/public.postcss'
 import '~at2@/foundations/at2-global.postcss'
 import { createApp } from 'vue'
+
 import TrackPlayer from './TrackPlayer'
-createApp(TrackPlayer).mount('#app')
+const app = createApp(TrackPlayer)
+import i18n from './pulgIn-i18n'
+app.use(i18n)
+app.mount('#app')
