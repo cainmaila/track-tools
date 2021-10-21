c7ecf37469c3bb788c07ab8c9b05c00aea3a590b
cainmaila <cainmaila@gmail.com>
2021-10-21 11:28:24 +0800

    fix use i18n

--- src/main-player-poc.js
+++ src/main-player-poc.js
@@ -2,4 +2,7 @@ import '@/assets/css/public.postcss'
 import '~at2@/foundations/at2-global.postcss'
 import { createApp } from 'vue'
 import PlayerPOC from './PlayerPOC'
-createApp(PlayerPOC).mount('#app')
+const app = createApp(PlayerPOC)
+import i18n from './pulgIn-i18n'
+app.use(i18n)
+app.mount('#app')
